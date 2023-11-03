2719c2530bd1c1f627c993df61a3eef851ea8b3f
RIvanCF <flores.ivan940214@gmail.com>
2023-11-03 00:14:57 -0600

    Operadores relacionales - Asistencia: 3 Noviembre 2023

---
+++ operadores_relacionales_logicos.py
@@ -0,0 +1,20 @@
+'''
+Los operadores relacionales son los siguientes: 
+    * > : Mayor que ...
+    * < : Menor que ...
+    * >= : Mayor o igual que ... 
+    * <= : Menor o igual que ... 
+    * == : Igual a ... 
+    * != : Diferente ...
+Nota: Doble signo igual (==) para los operadores relacionales es a una equivalencia entre dos valores del mismo valor, mientras que un signo igual (=) es una asignación
+de datos.
+
+Al comparar dos valore con estos aoperadores, la consola nos soltará un valor Booleano 
+'''
+number_1 = 40 
+number_2 = 70
+result_1 = 40 > 70
+result_2 = 70 > 40
+print("Operador 'Mayor que...' (>)")
+print("¿",number_1,"es mayor que ", number_2,"? ",result_1)
+print("¿",number_2,"es mayor que", number_1,"? ",result_2,"\n")
